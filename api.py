c5d50d386516a209eaa6eaeec07d605cdb541320
luizalves03-003 <luizcauaalves03@gmail.com>
2026-07-02 08:40:31 -0300

    Seu comentário aqui

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, url_for, redirect
 import sqlite3
 
 app = Flask(__name__)
 DATABASE = 'biblioteca.db'
+app.secret_key = 'sua_chave_secreta_aqui'
+
 
 def init_db():
     with sqlite3.connect(DATABASE) as conn:
@@ -11,21 +13,23 @@ def init_db():
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 nome TEXT NOT NULL,
                 genero TEXT NOT NULL,
-                preco REAL NOT NULL
+                ano INTEGER NOT NULL
             )
         ''')
         print("\n ---- BIBLIOTECA ABERTA ---- ")
 
-@app.route('/livros', methods=['GET'])
-def listar_livros():
+
+@app.route('/', methods=['GET'])
+def pagina_inicial():
     with sqlite3.connect(DATABASE) as conn:
         conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
         livros = cursor.execute("SELECT * FROM  livros").fetchall()
 
         resultado = [dict(row) for row in livros]
-        return jsonify(resultado)
-    
+        return render_template('index.html', resultados=resultado)
+
+
 @app.route('/livros/<int:id>', methods=['GET'])
 def livro_id(id):
     with sqlite3.connect(DATABASE) as conn:
@@ -37,38 +41,95 @@ def livro_id(id):
 
         if livro_row:
             return jsonify(dict(livro_row))
-        return jsonify({'erro': 'Livro não encontrdo'}), 404
+        return jsonify({'erro': 'Livro não encontrado'}), 404
+
+
+@app.route('/livros/genero/<string:genero_nome>', methods=['GET'])
+def genero_livros(genero_nome):
+    with sqlite3.connect(DATABASE) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM livros WHERE genero = ?", (genero_nome,))
+        livros_row = cursor.fetchall()
+
+        if livros_row:
+            resultado = [dict(row) for row in livros_row]
+            return jsonify(resultado)
+        return jsonify({'erro': 'livro não encontrado'}), 404
+
+
+@app.route('/livros/editar/<int:id>', methods=['GET'])
+def pagina_editar(id):
+
+    with sqlite3.connect(DATABASE) as conn:
+        conn.row_factory = sqlite3.Row
+        cursor = conn.cursor()
+        livro = cursor.execute(
+            "SELECT * FROM livros WHERE id=?", (id,)).fetchone()
+
+    if livro is None:
+        return "Livro não encontrado", 404
+
+    return render_template("editar.html", livro=dict(livro))
+
 
 @app.route('/livros', methods=['POST'])
 def adicionar_livro():
-    dados = request.json
+    nome = request.form.get('nome')
+    genero = request.form.get('genero')
+    try:
+        ano = int(request.form.get('ano'))
+    except (ValueError, TypeError):
+        return jsonify({'erro': 'Erro: O ano deve ser um número válido'}), 404
+
     with sqlite3.connect(DATABASE) as conn:
         cursor = conn.cursor()
+        if not nome or not genero:
+            return jsonify({'erro': 'Todos os campos são obrigatórios'}), 400
         cursor.execute(
-            "INSERT INTO livros (nome, genero, preco) VALUES (?,?,?)",
-            (dados['nome'], dados['genero'], dados['preco'])
+            "INSERT INTO livros (nome, genero, ano) VALUES (?,?,?)",
+            (nome, genero, ano)
         )
-        conn.commit()
-    return jsonify({"mensagem": "livro adicionado!"}),201
 
-@app.route('/livros/<int:id>', methods=['PUT'])
-def ataulizar_livros(id):
-    dados = request.json
+        conn.commit()
+    return redirect(url_for('pagina_inicial'))
+
+
+@app.route('/livros/atualizar/<int:id>', methods=['POST'])
+def atualizar_livros(id):
+    nome = request.form.get('nome')
+    genero = request.form.get('genero')
+    try:
+        ano = int(request.form.get('ano'))
+    except (ValueError, TypeError):
+        return jsonify({'erro': 'O ano deve ser um número válido'}), 404
+
     with sqlite3.connect(DATABASE) as conn:
-        conn.execute(
-            "UPDATE livros SET nome=?, genero=?, preco=? WHERE id=?",
-            (dados['nome'], dados['genero'], dados['preco'], id)
+        cursor = conn.cursor()
+        if not nome or not genero:
+            return jsonify({'erro': 'Todos os campos são obrigatórios'}), 400
+        cursor.execute(
+            "UPDATE livros SET nome=?, genero=?, ano=? WHERE id=?",
+            (nome, genero, ano, id)
         )
-        conn.commit()
-    return jsonify({"mensagem":"livro atualizado!"})
+        if cursor.rowcount == 0:
+            return jsonify({'erro': 'Livro não encontrado'}), 404
 
-@app.route('/livros/<int:id>', methods=['DELETE'])
+        conn.commit()
+        return redirect(url_for('pagina_inicial'))
+
+
+@app.route('/livros/deletar/<int:id>', methods=['POST'])
 def deletar_livro(id):
     with sqlite3.connect(DATABASE) as conn:
-        conn.execute("DELETE FROM livros WHERE id=?",(id,))
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM livros WHERE id=?", (id,))
+        if cursor.rowcount == 0:
+            return jsonify({'erro': 'Livro não encontrado'}), 404
         conn.commit()
-    return jsonify({"mensagem":"livro removido!"})
+    return redirect(url_for('pagina_inicial'))
+
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port="5454")
